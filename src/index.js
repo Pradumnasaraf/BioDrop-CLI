1ad44b9c1d3d661e13bf3fc080bc15b6e7638aff
Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
2022-09-01 08:20:17 +0000

    feat: check if user exists

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 #! /usr/bin/env node
 
 const chalk = require("chalk");
+const { create } = require("domain");
 const { prompt } = require("enquirer");
 const fs = require("fs");
 const createUser = require("./util/createUser");
 
-function questions() {
+function start() {
   prompt([
     {
       type: "input",
@@ -21,37 +22,68 @@ function questions() {
             ` File with ${answers.name}.json already exists, try again! `
           )
         );
-        questions();
+        start();
       } else {
-        prompt([
-          {
-            type: "input",
-            name: "name",
-            message: "What is your name?",
-          },
-          {
-            type: "select",
-            name: "type",
-            message: "Your profile type?",
-            choices: ["Personal", "Community"],
-          },
-          {
-            type: "input",
-            name: "bio",
-            message: "Add a short bio about yourself",
-          },
-        ])
-          .then((answers) => {
-            createUser(GHUsername, answers);
-          })
-          .catch((err) => {
-            console.log(err);
-          });
+        checkUser(GHUsername).then((result) => {
+          if (result === true) {
+            questions(GHUsername);
+          } else {
+            console.log(
+              chalk.bgRed.bold(
+                ` User with username ${GHUsername} does not exist, try again! `
+              )
+            );
+            start();
+          }
+        });
       }
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+}
+
+function questions(username) {
+  prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is your name?",
+    },
+    {
+      type: "select",
+      name: "type",
+      message: "Your profile type?",
+      choices: ["Personal", "Community"],
+    },
+    {
+      type: "input",
+      name: "bio",
+      message: "Add a short bio about yourself",
+    },
+  ])
+    .then((answers) => {
+      createUser(username, answers);
     })
     .catch((err) => {
       console.log(err);
     });
 }
 
-questions();
+async function checkUser(username) {
+  const axios = require("axios");
+  try {
+    const response = await axios.get(
+      `https://api.github.com/users/${username}`
+    );
+    if (response.status === 200) {
+      return true;
+    } else {
+      return false;
+    }
+  } catch (error) {
+    return false;
+  }
+}
+
+start();
