49a2b672fbab530b34f10701fd3331a97601bf75
Pradumna Saraf <pradumnasaraf@gmail.com>
2023-02-22 23:46:48 +0530

    feat: Add bug report from CLI

---
+++ src/reportbug/reportBug.js
@@ -0,0 +1,46 @@
+const open = require("open");
+const enquirer = require("enquirer");
+
+async function reportBug() {
+  const questions = await enquirer.prompt([
+    {
+      type: "input",
+      name: "username",
+      message: "What is your GitHub username?",
+    },
+    {
+      type: "input",
+      name: "title",
+      message: "What is the title of the bug?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "Describe a description of the bug",
+    },
+  ]);
+  const { username, title, description } = questions;
+  const url = urlBuilder(username, title, description);
+
+  const openBrowser = await enquirer.prompt([
+    {
+      type: "confirm",
+      name: "openBrowser",
+      message:
+        "Thank you for taking the time to report the bug. For submitting it, you will be redirected to GitHub Issues. Do you want to open the browser?",
+    },
+  ]);
+  if (openBrowser.openBrowser) {
+    await open(url);
+  }
+}
+
+function urlBuilder(username, title, description) {
+  title = title.replace(/\s/g, "+");
+  description = description.replace(/\s/g, "+");
+
+  URL = `https://github.com/Pradumnasaraf/LinkFree-CLI/issues/new?assignees=&labels=bug&template=bug.yaml&title=${title}+Report+by+@${username}&description=${description}`;
+  return URL;
+}
+
+module.exports = reportBug;
