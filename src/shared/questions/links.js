c247aae9016b3fe3d4dcda89b4111284e801e465
Pradumna Saraf <pradumnasaraf@gmail.com>
2023-06-02 10:38:40 +0530

    feat: Add color and group property in links (#61)
    Co-authored-by: Pratik Dey <56970651+PratikDey@users.noreply.github.com>
    Co-authored-by: Pradumnasaraf <Pradumnasaraf@users.noreply.github.com>

--- src/shared/questions/links.js
+++ src/shared/questions/links.js
@@ -15,6 +15,16 @@ async function addlinks(bool) {
         name: "url",
         message: "What is the URL of the link?",
       },
+      {
+        type: "input",
+        name: "addColor",
+        message: "Give a color theme. (Optional. Press enter to skip.)",
+      },
+      {
+        type: "input",
+        name: "addGroup",
+        message: "Give a group name. (Optional. Press enter to skip.)",
+      },
     ]);
     let selectedIcon = await selecticon();
     const confirm = await prompt([
@@ -29,6 +39,15 @@ async function addlinks(bool) {
       url: answers.url,
       icon: selectedIcon,
     });
+
+    // Check if user wants to add color or group name
+    if (answers.addColor !== "") {
+      links[links.length - 1].color = answers.addColor;
+    }
+    if (answers.addGroup !== "") {
+      links[links.length - 1].group = answers.addGroup;
+    }
+
     if (!confirm.addLink) {
       return links;
     }
@@ -82,7 +101,7 @@ async function updatelinks(links) {
         message: "Choose which one you want to update",
       },
     ]);
-    const { name, url } = await prompt([
+    const { name, url, color, group } = await prompt([
       {
         type: "input",
         name: "name",
@@ -93,6 +112,17 @@ async function updatelinks(links) {
         name: "url",
         message: "What is the new URL of the link?",
       },
+      {
+        type: "input",
+        name: "color",
+        message:
+          "What is the new color theme? (Optional. Press enter to skip.)",
+      },
+      {
+        type: "input",
+        name: "group",
+        message: "What is the new group name? (Optional. Press enter to skip.)",
+      },
     ]);
     const selectedIcon = await selecticon();
     const { updateLink } = await prompt([
@@ -102,18 +132,28 @@ async function updatelinks(links) {
         message: "Do you want to update another link?",
       },
     ]);
+
+    // Update the link properties with the new values
     choiceLinks.map((link) => {
       if (link.name === answers.link) {
         link.name = name;
         link.url = url;
         link.icon = selectedIcon;
+        if (color !== "") {
+          link.color = color;
+        }
+        if (group !== "") {
+          link.group = group;
+        }
       }
     });
+
+    // If user doesn't want to update another link, return the updated links
     if (!updateLink) {
       let result = [];
       choiceLinks.map((link) => {
-        const { name, url, icon } = link;
-        result.push({ name, url, icon });
+        const { name, url, icon, color, group } = link;
+        result.push({ name, url, icon, color, group });
       });
       return result;
     }
